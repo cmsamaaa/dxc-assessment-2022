4cd3e98b137c50e1a5f60356c7bbe4e9af8603c0
Loh Chun Mun <chunmun_loh@live.com>
2022-10-06 05:56:40 +0800

    start batch script

---
+++ backend/start-backend.bat
@@ -0,0 +1,5 @@
+@REM Required ENV vars:
+@REM JAVA_HOME - location of a JDK 17 home dir
+set JAVA_HOME=C:\Program Files\Java\jdk-17.0.2
+
+.\mvnw spring-boot:run
---
+++ run.bat
@@ -0,0 +1,9 @@
+@REM cd .\frontend
+@REM call npm start
+
+cd .\backend
+start "New Window" cmd /c mvnw spring-boot:run
+
+pause
+
+exit
