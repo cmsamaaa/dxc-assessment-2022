a4dd1f79c103393630001065b74e412c50a82f24
Loh Chun Mun <chunmun_loh@live.com>
2022-10-06 19:02:50 +0800

    rename package and added CORS

--- backend/src/main/java/com/dxc/assessment/backend/Controller/LoginController.java
+++ backend/src/main/java/com/dxc/assessment/backend/Controller/LoginController.java
@@ -1,11 +1,13 @@
-package com.dxc.assessment.backend.Controllers;
+package com.dxc.assessment.backend.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dxc.assessment.backend.Models.Account;
+import com.dxc.assessment.backend.Model.Account;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class LoginController {
 	
--- backend/src/main/java/com/dxc/assessment/backend/Model/Account.java
+++ backend/src/main/java/com/dxc/assessment/backend/Model/Account.java
@@ -1,4 +1,4 @@
-package com.dxc.assessment.backend.Models;
+package com.dxc.assessment.backend.Model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
